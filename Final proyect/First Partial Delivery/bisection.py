39eca969c532f5958d9c66ee841543203e4502ef
eugenioLeal <eugenio.leal.333@gmail.com>
2018-09-01 13:44:14 -0500

    Added my bisection and false position methods :boom:

--- Final proyect/First Partial Delivery/bisection.py
+++ Final proyect/First Partial Delivery/bisection.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 
 def f(x):
-    return x**3 +1
+    return ((668.06 / x) * (1 - (exp(1)**(-0.146843*x)) - 40))
 def bisection(a,b,tol):
     xl = a
     xr = b
-    while (np.abs(xl-xr)>= tol):
+    while (np.abs(xl-xr) >= tol):
         c = (xl+xr)/2.0
         prod = f(xl)*f(c)
         if prod > tol:
@@ -22,7 +22,7 @@ def bisection(a,b,tol):
                 xr = c
     return c
 
-answer = bisection(-5,5,1e-8)
+answer = bisection(12,16,5e-1)
 print(" bisection method gives root at x = ", answer)
 x = np.linspace(-2,2,5)
 plt.plot(x,f(x))
